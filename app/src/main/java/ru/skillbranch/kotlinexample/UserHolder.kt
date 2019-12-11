fdc096e552f33ccb85684ebbe5edb80c224e339d
Sheykin <sheik80@gmail.com>
2019-12-11 17:17:38 +0900

    hometask_2

--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -1,4 +1,38 @@
 package ru.skillbranch.kotlinexample
 
+import android.provider.ContactsContract
+
 object UserHolder {
+    private val map = mutableMapOf<String, User >()
+
+    fun registerUser(
+        fullName:String,
+        email: String,
+        password: String
+    ): User{
+        return User.makeUser(fullName, email = email, password = password)
+            .also { user -> map[user.login] = user }
+    }
+
+    fun loginUser(login:String, password: String): String? {
+        return map[login.trim()]?.run{
+            if (checkPassword(password)) this.userInfo
+            else null
+        }
+    }
+
+    fun registerUserByPhone(
+        fullName: String,
+        rawPhone: String
+    ) : User{
+        return User.makeUser(fullName,rawPhone)
+    }
+
+    fun  requestAccessCode(login: String, password: String
+    ) : String? {
+        return map[login.trim()]?.run{
+            if (checkPassword(password)) this.userInfo
+            else null
+        }
+    }
 }
